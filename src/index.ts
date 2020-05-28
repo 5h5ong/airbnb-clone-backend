4cad9083250598dea83674c7ad021c80c48194b4
Unknown <Call5h5ong@gmail.com>
2020-05-28 12:24:21 +0900

    passport를 이용해 jwt 인증 구현
    `passport`, `passport-jwt` 설치함.
    jwt 인증 구현함.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { createConnection } from 'typeorm';
 import cors from 'cors';
+import passport from 'passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
 import onlineExperiencesController from './controller/onlineExperiences/onlineExperiencesController';
 import createUsersController from './controller/users/createUsersController';
 import VerifyUserController from './controller/users/VerifyUserController';
+import createRepository from './functions/createRepository/createRepository';
+import Users from './entity/Users';
 
 const port = 4000;
 const app = express();
@@ -13,6 +17,29 @@ app.use(cors());
 // body json 설정
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// passport 설정
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET_KEY,
+};
+passport.use(
+  new Strategy(opts, async (jwtPayload, done) => {
+    const userRepo = createRepository<Users>(Users);
+
+    try {
+      const user = await userRepo.findOne(jwtPayload.id);
+      if (user) {
+        return done(null, user);
+      }
+      return done(null, false, { message: '유저가 존재하지 않습니다.' });
+    } catch (error) {
+      return done(error, false, {
+        message: '인증 과정에서 에러가 발생하였습니다.',
+      });
+    }
+  })
+);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.post('/verify/user', VerifyUserController);
